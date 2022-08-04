c21e81ecc8d4558552be89464a7096ec44a13a76
zkk2157 <1066830769@qq.com>
2022-08-04 14:05:36 +0800

    前后端联动

---
+++ src/service/sendFiles.js
@@ -0,0 +1,7 @@
+// import http from "./index.js";
+
+// function loginService(pathParams, username, password, data) {
+//     return http.post(`/pet/${pathParams}`, {
+//         body: '这里放body'
+//     }, { params: { username: '这里放查询参数' }, headers: { name: '这里放headers' } })
+// }
